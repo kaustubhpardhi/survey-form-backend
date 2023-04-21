943cf2cebb11c00b3961fb739ed1b395bfd112dc
Kaustubh Pardhi <kaustubhpardhi006@gmail.com>
2023-04-21 15:52:53 +0530

    core:r redis-createclient

--- Config/redis.config.js
+++ Config/redis.config.js
@@ -14,12 +14,20 @@
 // // Export the Redis client instance
 // module.exports = redisClient;
 const redis = require("redis");
+const createClient = redis.createClient;
 
-// Create a Redis client instance
-const redisClient = redis.createClient({
-  host: "redis-18359.c17.us-east-1-4.ec2.cloud.redislabs.com:18359",
-  port: 12345,
+const redisClient = createClient({
+  password: "8nNfSVDOVnzfmtGX3iNLwHtpEdbYRNsz",
+  socket: {
+    host: "redis-18359.c17.us-east-1-4.ec2.cloud.redislabs.com",
+    port: 18359,
+  },
 });
+// Create a Redis client instance
+// const redisClient = redis.createClient({
+//   host: "redis-18359.c17.us-east-1-4.ec2.cloud.redislabs.com:18359",
+//   port: 12345,
+// });
 (async () => {
   await redisClient.connect();
 })();
