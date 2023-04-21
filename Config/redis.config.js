b04e5c114a8738dc2f3f3d4c011678b04074924c
Kaustubh Pardhi <kaustubhpardhi006@gmail.com>
2023-04-21 15:43:28 +0530

    core: redis labs

--- Config/redis.config.js
+++ Config/redis.config.js
@@ -1,14 +1,31 @@
+// const redis = require("redis");
+
+// // Create a Redis client instance
+// const redisClient = redis.createClient({
+//   host: "redis-server",
+//   port: 6379,
+// });
+
+// // Listen for errors
+// redisClient.on("error", (error) => {
+//   console.log(error);
+// });
+
+// // Export the Redis client instance
+// module.exports = redisClient;
 const redis = require("redis");
 
 // Create a Redis client instance
 const redisClient = redis.createClient({
-  host: "https://surveyformbackend-98c8.onrender.com",
-  port: 6379,
+  host: "redis-18359.c17.us-east-1-4.ec2.cloud.redislabs.com:18359",
+  port: 12345,
 });
-
+(async () => {
+  await redisClient.connect();
+})();
 // Listen for errors
 redisClient.on("error", (error) => {
-  console.log(error);
+  console.error(error);
 });
 
 // Export the Redis client instance
